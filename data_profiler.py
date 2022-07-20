c0a960287a6450bdf6b1392261946907800b2279
Preksha Agarwal <pragarwal@oti.nyc.gov>
2022-07-20 20:51:13 +0000

    create directory structure for each day and get for loop working

--- data_profiler.py
+++ data_profiler.py
@@ -10,9 +10,45 @@ import gspread
 from pathlib import Path
 import os 
 
-# This will point to mnt/ NOT the directory where this file lives (mnt/notebooks/scratch), when run as a Domino Job 
-current_dir = os.getcwd()
+import time, datetime, os
+#import re 
+import shutil 
+#import pdfkit as pdf
+#from IPython.core.display import display, HTML
 
+import streamlit as st
+
+from streamlit_pandas_profiling import st_profile_report
+
+
+# Check today's date and create a directory file structure for today's date.
+today = datetime.date.today()  
+todaystr = today.isoformat()   
+
+def create_date_dir():
+    i = 0
+    d = todaystr
+    if not os.path.exists(d):
+        os.mkdir(d)
+    else:
+        '''#if os.path.exists(d):
+        #    i += 1
+        #    d = d[:10] + '_' + str(i).zfill(2)
+        #os.mkdir(d)
+        #else:
+        #    m = re.findall(r'\w\w$', d)
+        #    i = float(m[0:]) + 1
+        #    d = d[:10] + '_' + str(i).zfill(2)
+        #os.mkdir(d)'''
+        shutil.rmtree(d)
+        #os.rmdir(d)
+        os.mkdir(d)
+        
+create_date_dir()
+
+# This will point to where the job is being run from NOT the directory where this file lives (mnt/notebooks/scratch), when run as a Domino Job 
+current_dir = os.getcwd()
+    
 ## Read in service account credentials - link to whichever json file holds your credentials
 gc = gspread.service_account(filename='/mnt/data/odt-data-profiler-069defc7abf3.json')
 
@@ -23,6 +59,7 @@ sh2 = gc.open_by_key('1gtMHAQkfufSOPb3hpThM_aZ0rwV8NG-CeVh3BY2dorQ')
 worksheet2 = sh2.get_worksheet(0)
 list_of_lists = worksheet2.get_all_records()
 
+
 # Profile each dataset
 def profile_data(list_of_lists):
     for i in range (0,len(list_of_lists)):
@@ -30,12 +67,22 @@ def profile_data(list_of_lists):
         dataset_url = list_of_lists[i]['url']
         df = pd.read_json(dataset_url)
         prl = ProfileReport(df, title=f"{dataset_name}_Data_Profiler", html={"style": {"full_width": True}}, sort=None)
-        prl.to_file(f"testing_{dataset_name}.html")
-        #prl.to_file(f"testing_{dataset_name}.html")
-    
+        #st.title(f"{dataset_name} in Streamlit!")
+        #st.write(df)
+        #st_profile_report(prl)
+        out = "Hello"
+        with open(Path(current_dir, todaystr, f'{dataset_name}.html'), 'w+') as outpath:
+            outpath.write(out)
+        #prl.to_file("test.html")
+        #out = prl.to_file(f"testing_{dataset_name}.json")
+        #with open(Path(current_dir, todaystr, f'{prl}.html'), 'w+') as outpath:
+        #    outpath.write(out)
+        #prl.to_html(current_dir + "/" + "test.html")
+        #prl.to_html("test.html")
+        #pdf.from_file('test.html', 'test.pdf')
+        #report_html = prl._render_html()
+        #display(HTML(report_html))
 # Run function
-out = profile_data(list_of_lists)
+#out = profile_data(list_of_lists)
 
-with open(Path(current_dir, 'notebooks', 'scratch', 'outputs', f'{out}.html'), 'w') as outpath:
-    outpath.write(out)
-    
+profile_data(list_of_lists)
--- profiler.sh
+++ profiler.sh
@@ -3,5 +3,8 @@ set -x
 
 sudo pip install pandas_profiling
 sudo pip install gspread
+sudo pip install pdfkit
+sudo pip install streamlit
+sudo pip install streamlit_pandas_profiling
 
 python data_profiler.py
