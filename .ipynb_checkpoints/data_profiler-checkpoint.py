23cb1cdf5683d901e111303c3e0c58061e782bc3
Preksha Agarwal <pragarwal@oti.nyc.gov>
2022-07-22 20:56:39 +0000

    cleanup .py

--- .ipynb_checkpoints/data_profiler-checkpoint.py
+++ .ipynb_checkpoints/data_profiler-checkpoint.py
@@ -1,25 +1,14 @@
 import pandas as pd
 import pandas_profiling
 from pandas_profiling import ProfileReport
-
 import numpy as np
 import string
 from google.oauth2 import service_account
 import gspread
-
 from pathlib import Path
 import os 
-
 import time, datetime, os
-#import re 
 import shutil 
-#import pdfkit as pdf
-#from IPython.core.display import display, HTML
-
-import streamlit as st
-
-from streamlit_pandas_profiling import st_profile_report
-
 
 # Check today's date and create a directory file structure for today's date.
 today = datetime.date.today()  
@@ -53,39 +42,33 @@ current_dir = os.getcwd()
 gc = gspread.service_account(filename='/mnt/data/odt-data-profiler-069defc7abf3.json')
 
 ## Spreadsheet key of your destination 
-sh2 = gc.open_by_key('1gtMHAQkfufSOPb3hpThM_aZ0rwV8NG-CeVh3BY2dorQ')
+sh = gc.open_by_key('1gtMHAQkfufSOPb3hpThM_aZ0rwV8NG-CeVh3BY2dorQ')
 
 # Read data from worksheet
-worksheet2 = sh2.get_worksheet(0)
-list_of_lists = worksheet2.get_all_records()
+worksheet = sh.get_worksheet(0)
+list_of_lists = worksheet.get_all_records()
 
 
 # Profile each dataset
-def profile_data(list_of_lists, todaystr):
+def profile_data(list_of_lists):
     for i in range (0,len(list_of_lists)):
-        #today = datetime.date.today()  
-        #todaystr = today.isoformat()
         agency_name = list_of_lists[i]['agency']
         dataset_name = list_of_lists[i]['dataset']
         dataset_url = list_of_lists[i]['url']
         df = pd.read_json(dataset_url)
-        prl = ProfileReport(df, title=f"{dataset_name}_Data_Profiler", html={"style": {"full_width": True}}, sort=None)
-        #st.title(f"{dataset_name} in Streamlit!")
-        #st.write(df)
-        #st_profile_report(prl)
-        #out = "Hello"
-        #with open(Path(current_dir, todaystr, f'{dataset_name}.html'), 'w+') as outpath:
-        #    outpath.write(out)
-        out = prl.to_file(f"{todaystr}_{agency_name}_{dataset_name}.html")
-        #out = prl.to_file("/mnt/nyc-odt-data-profiling/f"{todaystr}_{agency_name}_{dataset_name}.html"")
+        prl = ProfileReport(df, title=f"{dataset_name}_Data_Profiler", html={"style": {"full_width": True}}, sort=None,
+            correlations=None,
+            interactions=None,
+            missing_diagrams={
+                "bar": False,
+                "matrix": True,
+                "heatmap": False,
+                "dendrogram": False,
+            })
+        out = prl.to_file(current_dir + "/" + todaystr + "/" + f"{todaystr}_{agency_name}_{dataset_name}.html")
         #with open(Path(current_dir, todaystr, f'{agency_name}_{dataset_name}.html'), 'w') as outpath:
         #    outpath.write(prl)
         #prl.to_html(current_dir + "/" + "test.html")
-        #prl.to_html("test.html")
-        #pdf.from_file('test.html', 'test.pdf')
-        #report_html = prl._render_html()
-        #display(HTML(report_html))
-# Run function
-#out = profile_data(list_of_lists)
 
-profile_data(list_of_lists, todaystr)
+# Run function
+profile_data(list_of_lists)
--- profiler.sh
+++ profiler.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 set -x
 
 #sudo pip install pandas_profiling==3.1.0
@@ -8,6 +8,12 @@ set -x
 #sudo pip install streamlit_pandas_profiling
 #sudo pip install cairosvg
 
-#python data_profiler.py
+python data_profiler.py
 
-mv /repos/nyc-odt-data-profiling/*.html /pagarwal/nyc-odt-data-profiling/.
+#mv /repos/nyc-odt-data-profiling/*.html /mnt/.
+
+#echo "      date     time $(free -m | grep total | sed -E 's/^    (.*)/\1/g')"
+#while true; do
+#    echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g')"
+#    sleep 1
+#done
