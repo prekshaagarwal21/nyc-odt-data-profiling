0136bdfa3d9fb1fd22b5e593616c541fd691ef99
Preksha Agarwal <pragarwal@oti.nyc.gov>
2022-08-08 15:23:39 +0000

    add more comments to .py and code cleanup

--- data_profiler.py
+++ data_profiler.py
@@ -11,32 +11,35 @@ import time, datetime, os
 import shutil
 import requests
 
-# Check today's date and create a directory file structure for today's date.
+## Check today's date and create a directory file structure for today's date.
 today = datetime.date.today()  
 todaystr = today.isoformat()   
 
 def create_date_dir():
     i = 0
     d = todaystr
-    if not os.path.exists(d):
+    if not os.path.exists(d): # if directory for today's date does not exist, create one.
         os.mkdir(d)
     else:
         shutil.rmtree(d)
         os.mkdir(d)
         
+## call create_date_dir function to create the directory structure for today's date.        
 create_date_dir()
 
-# This will point to where the job is being run from NOT the directory where this file lives (mnt/notebooks/scratch), when run as a Domino Job 
+## This will point to where the job is being run from NOT the directory where this file lives, when run as a Domino Job. 
+## In this case the job is being run from the same directory (/repos/nyc-odt-data-profiling) where the file lives.
 current_dir = os.getcwd()
     
-## Read in service account credentials - link to whichever json file holds your credentials
+## Read in service account credentials - link to json file that holds GCP credentials.
 gc = gspread.service_account(filename='/mnt/data/odt-data-profiler-069defc7abf3.json')
 
-## Spreadsheet key of your destination 
+## Spreadsheet key of your destination. This is the working input document used by the Open Data team. 
+## (link here: https://docs.google.com/spreadsheets/d/1gtMHAQkfufSOPb3hpThM_aZ0rwV8NG-CeVh3BY2dorQ/edit#gid=0) 
 sh = gc.open_by_key('1gtMHAQkfufSOPb3hpThM_aZ0rwV8NG-CeVh3BY2dorQ')
 
-# Read data from worksheet
-worksheet = sh.get_worksheet(0)
+# Read data from "data to profile" tab of the worksheet. 
+worksheet = sh.worksheet("data to profile")
 list_of_lists = worksheet.get_all_records()
 
 
@@ -53,7 +56,7 @@ def profile_data(list_of_lists):
         tmp_dfs = []
         while not reached_end_of_dataset:
             chunk_url = f'{dataset_url}?$limit={chunk_size}&$offset={offset}'
-            tmp_r = requests.get(chunk_url, auth=("8febnpgtxedbep4no9oqegrce","36g68l45jhsdzeu08j3i44j4nxn2t65ua1gw4o0xwa9o17ownv"), timeout=None) #pd.read_json(chunk_url)
+            tmp_r = requests.get(chunk_url, auth=("8febnpgtxedbep4no9oqegrce","36g68l45jhsdzeu08j3i44j4nxn2t65ua1gw4o0xwa9o17ownv"), timeout=None)
             tmp = pd.DataFrame(tmp_r.json())
             tmp_dfs.append(tmp)
     
@@ -64,7 +67,6 @@ def profile_data(list_of_lists):
                 reached_end_of_dataset = True
 
         out_df = pd.concat(tmp_dfs, axis=0)
-        #r = requests.get(dataset_url+f"?$limit=500000", auth=("8febnpgtxedbep4no9oqegrce","36g68l45jhsdzeu08j3i44j4nxn2t65ua1gw4o0xwa9o17ownv"), timeout=None)
         prl = ProfileReport(out_df, title=f"{dataset_name}_Data_Profiler", html={"style": {"full_width": True}}, sort=None,
             correlations=None,
             interactions=None,
@@ -75,9 +77,6 @@ def profile_data(list_of_lists):
                 "dendrogram": False,
             })
         out = prl.to_file(current_dir + "/" + todaystr + "/" + f"{todaystr}_{agency_name}_{dataset_name}.html")
-        #with open(Path(current_dir, todaystr, f'{agency_name}_{dataset_name}.html'), 'w') as outpath:
-        #    outpath.write(prl)
-        #prl.to_html(current_dir + "/" + "test.html")
 
 # Run function
 profile_data(list_of_lists)
--- profiler.sh
+++ profiler.sh
@@ -1,19 +1,7 @@
 #!/bin/sh -e
 set -x
 
-sudo pip install pandas_profiling==3.1.0
+sudo pip install pandas_profiling==3.1.0 ## this is the version of pandas_profiling that does not result in errors with write to_html. 
 sudo pip install gspread
-#sudo pip install pdfkit
-#sudo pip install streamlit
-#sudo pip install streamlit_pandas_profiling
-#sudo pip install cairosvg
 
 python /repos/nyc-odt-data-profiling/data_profiler.py
-
-#mv /repos/nyc-odt-data-profiling/*.html /mnt/.
-
-#echo "      date     time $(free -m | grep total | sed -E 's/^    (.*)/\1/g')"
-#while true; do
-#    echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g')"
-#    sleep 1
-#done
