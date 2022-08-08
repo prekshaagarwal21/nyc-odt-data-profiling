56c4929da5188bf665b5e44280f2eec927e8d236
Preksha Agarwal <pragarwal@oti.nyc.gov>
2022-08-08 17:45:14 +0000

    add more comments to code

--- .ipynb_checkpoints/data_profiler-checkpoint.py
+++ .ipynb_checkpoints/data_profiler-checkpoint.py
@@ -40,17 +40,17 @@ sh = gc.open_by_key('1gtMHAQkfufSOPb3hpThM_aZ0rwV8NG-CeVh3BY2dorQ')
 
 # Read data from "data to profile" tab of the worksheet. 
 worksheet = sh.worksheet("data to profile")
-list_of_lists = worksheet.get_all_records()
+list_of_lists = worksheet.get_all_records() ## this returns a list of lists containing key/value pairs for each field.
 
 
 # Function to profile each dataset
 def profile_data(list_of_lists):
     for i in range (0,len(list_of_lists)):
-        agency_name = list_of_lists[i]['agency']
-        dataset_name = list_of_lists[i]['dataset']
-        url = list_of_lists[i]['4x4']
-        dataset_url = f'https://data.cityofnewyork.us/resource/{url}.json'
-        chunk_size = 100000
+        agency_name = list_of_lists[i]['agency'] # agency field for each row - will be used in the report name
+        dataset_name = list_of_lists[i]['dataset'] # dataset name for each row - will be used in the report name
+        url = list_of_lists[i]['4x4'] # 4x4 for each row - will be used to identify the dataset and read in the data from the API endpoint
+        dataset_url = f'https://data.cityofnewyork.us/resource/{url}.json' # defining the url using the 4x4 for each row
+        chunk_size = 100000 # the chunk size is the limit that will be set for the get requests. Only 100000 rows are read at a given time. 
         offset = 0
         reached_end_of_dataset = False
         tmp_dfs = []
@@ -60,13 +60,13 @@ def profile_data(list_of_lists):
             tmp = pd.DataFrame(tmp_r.json())
             tmp_dfs.append(tmp)
     
-            if tmp.shape[0] == chunk_size:
+            if tmp.shape[0] == chunk_size: # Offset and limit are used together to incrementally read 100000 rows at a time if we have not reached the end of the dataset. 
                 offset += chunk_size
         
             else: # if got the last chunk, should be smaller than the chunk size 
                 reached_end_of_dataset = True
 
-        out_df = pd.concat(tmp_dfs, axis=0)
+        out_df = pd.concat(tmp_dfs, axis=0) # chunks of dataset are concatenated to get the full dataset. NOTE THAT THIS STEP FAILS FOR VERY LARGE DATASETS LARGER THAN ~ 10 MILLION ROWS.
         prl = ProfileReport(out_df, title=f"{dataset_name}_Data_Profiler", html={"style": {"full_width": True}}, sort=None,
             correlations=None,
             interactions=None,
@@ -75,8 +75,8 @@ def profile_data(list_of_lists):
                 "matrix": True,
                 "heatmap": False,
                 "dendrogram": False,
-            })
-        out = prl.to_file(current_dir + "/" + todaystr + "/" + f"{todaystr}_{agency_name}_{dataset_name}.html")
+            }) # using pandas_profiling to generate the report. Many features are configurable within this function. For now, we only turn off some of the extraneous information that we don't want to see.
+        out = prl.to_file(current_dir + "/" + todaystr + "/" + f"{todaystr}_{agency_name}_{dataset_name}.html") # output the report to the appropriate directory.
 
 # Run function
-profile_data(list_of_lists)
+profile_data(list_of_lists) 
